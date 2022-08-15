becf471801f204473b1942ee321c1cda82aeef11
YooJinRa <rarastudy@naver.com>
2022-08-16 04:52:33 +0900

    ✨new : posting image resizing & validation check complete

--- src/components/posting/PostForm.jsx
+++ src/components/posting/PostForm.jsx
@@ -1,99 +1,126 @@
-import React, { useState, useRef } from "react";
+import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import { __addPost } from "../../redux/modules/postSlice";
+import imageCompression from "browser-image-compression";
+import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
-
 import IconImage from "../../static/icon_image.png";
 
 const PostForm = () => {
   const dispatch = useDispatch();
-  const brandSelect = useRef();
-  const menuSelect = useRef();
-  const contentTextarea = useRef();
-  
-  // ::: 이미지 미리보기(Image Preview) 기능 구현, 이미지 리사이징 해야함!!!
-  // :::: => User가 PostForm에서 사진 업로드 했을때만 확인하기에 해당 컨포넌트에서 상태관리 진행
-  const [ imgUrl, setImgUrl ] = useState(null);
+  const navigate = useNavigate();
 
-  const encodeFileToBase64 = (fileBlob) => {
-    const reader = new FileReader();
-    reader.readAsDataURL(fileBlob);
-    return new Promise((resolve, reject) => {
-      reader.onload = () => {
-        resolve(reader.result);
-      }
-      reader.onerror = () => {
-        console.log(reader.error);
-        reject(reader.error);
-        
-      }
+  // ::: 게시글 등록 폼 사용자 입력값 받아오기
+  const [inputs, setInputs] = useState({
+    brandId: '',
+    menuId: '',
+    content: ''
+  });
+  const [validationMessage, setValidationMessage] = useState([]);
+  const [ postImg, setPostImg ] = useState(null);
+  const { brandId, menuId, content } = inputs;
+
+  const onChangePostingForm = (event) => {
+    const { value, name } = event.target;
+    setInputs({
+      ...inputs,
+      [name] : value
     });
   }
+
+  // ::: 이미지 미리보기(Image Preview) 및 리사이징(Resizing) 구현
   const uploadImage = async (event) => {
-    console.log(event.target.files[0]);
-    const response = await encodeFileToBase64(event.target.files[0]);
-    setImgUrl(response);
+    const imageFile = event.target.files[0];
+    // :: 변환할 이미지 크기 지정
+    const options = {
+      maxSizeMB: 1,
+      maxWidthOrHeight: 1920,
+      useWebWorker: true
+    }
+    try {
+      const compressedFile = await imageCompression(imageFile, options);
+      const imageResult = await imageCompression.getDataUrlFromFile(compressedFile);
+      setPostImg(imageResult);   
+    } catch (error) {
+      console.log("__PostForm_ploadImage error ::", error);
+      alert("이미지를 업로드 하는데 문제가 생겼습니다. 다시 시도해주세요!");
+    }
   }
 
+  // ::: 게시글 등록
   const onClickAddPost = (event) => {
     event.preventDefault();
-    const emptyBox = [];
-    const brandId = brandSelect.current.value;
-    const menuId = menuSelect.current.value;
-    const content = contentTextarea.current.value;
-    
-    if(brandId === '' || menuId === '' || content === '' || imgUrl === null) {
+    setValidationMessage([]);
+    if(inputs.brandId === '' || inputs.menuId === '' || inputs.content === '' || postImg === null) {
       // ::: 유효성 검사
-      brandId === '' && emptyBox.push('브랜드');
-      menuId === '' && emptyBox.push('메뉴');
-      content === '' && emptyBox.push('내용');
-      imgUrl === null && emptyBox.push('사진');
-      console.log("아래의 내용도 채워주셔야 해요!", emptyBox);
+      inputs.brandId === '' && setValidationMessage((prev) => [...prev, '브랜드']);
+      inputs.menuId === '' && setValidationMessage((prev) => [...prev, '메뉴']);
+      inputs.content === '' && setValidationMessage((prev) => [...prev, '내용']);
+      postImg === null && setValidationMessage((prev) => [...prev, '사진']);
+      console.log("아래의 내용도 채워주셔야 해요!", validationMessage);
 
     } else {
       // ::: 게시글 등록
-      console.log(brandId, menuId, content, imgUrl);
       const newPost = {
-        brandId: Number(brandId),
-        menuId: Number(menuId),
-        content,
-        imageUrl: imgUrl
+        brandId: Number(inputs.brandId),
+        menuId: Number(inputs.menuId),
+        content: inputs.content,
+        postImg: postImg
       }
       dispatch(__addPost(newPost));
-      setImgUrl(null);
+      setPostImg(null);
+      setInputs({
+        brandId: '',
+        menuId: '',
+        content: ''
+      });
+      navigate(`/`);
     }
+  }
 
+  const onClickCancel = (event) => {
+    event.preventDefault();
+    navigate(-1);
   }
 
   return (
     <StPostFormWrap>
       <StPostImageBox>
-        {imgUrl && <img src={imgUrl} alt="preview" />}
+        { postImg
+        && <img src={postImg} alt="preview" /> }
       </StPostImageBox>
 
       <StPostContentsBox>
         <h2>경험을 공유해주세요!</h2>
         <StRowFormBox>
           <label>브랜드</label>
-          <select name="brand" ref={brandSelect}>
+          <select 
+            name='brandId' 
+            value={brandId} 
+            onChange={onChangePostingForm}
+          >
             <option value=''>브랜드를 선택해주세요!</option>
-            <option value='0' brandname='STARBUCKS'>스타벅스</option>
-            <option value='1' brandname='MEGA'>메가커피</option>
-            <option value='2' brandname='HOLLYS'>할리스커피</option>
-            <option value='3' brandname='PAIKS'>빽다방</option>
-            <option value='4' brandname='PAULBASSETT'>폴바셋커피</option>
+            <option value='1' brandname='STARBUCKS'>스타벅스</option>
+            <option value='2' brandname='MEGA'>메가커피</option>
+            <option value='3' brandname='HOLLYS'>할리스커피</option>
+            <option value='4' brandname='PAIKS'>빽다방</option>
+            <option value='5' brandname='PAULBASSETT'>폴바셋커피</option>
           </select>
         </StRowFormBox>
 
         <StRowFormBox>
           <label>메뉴</label>
-          <select name="menu" ref={menuSelect}>
+          <select 
+            name='menuId' 
+            value={menuId} 
+            onChange={onChangePostingForm}
+          >
             <option value=''>메뉴를 선택해주세요!</option>
-            <option value='0' menuname='AMERICANO'>아메리카노</option>
-            <option value='1' menuname='COLDBREW'>콜드브루</option>
-            <option value='2' menuname='CAFELATTE'>카페라떼</option>
-            <option value='3' menuname='CAPPUCCINO'>카푸치노</option>
-            <option value='4' menuname='MOCHALATTE'>카페모카라떼</option>
+            <option value='1' menuname='AMERICANO'>아메리카노</option>
+            <option value='2' menuname='COLDBREW'>콜드브루</option>
+            <option value='3' menuname='CAFELATTE'>카페라떼</option>
+            <option value='4' menuname='CAPPUCCINO'>카푸치노</option>
+            <option value='5' menuname='MOCHALATTE'>카페모카라떼</option>
           </select>
         </StRowFormBox>
 
@@ -101,7 +128,9 @@ const PostForm = () => {
           <label>내용</label>
           <textarea 
             placeholder="경험한 내용을 입력해주세요!"
-            ref={contentTextarea}
+            name='content' 
+            value={content} 
+            onChange={onChangePostingForm}
           ></textarea>
         </StRowFormBox>
         <StRowFormBox>
@@ -111,10 +140,24 @@ const PostForm = () => {
             accept="image/jpg, image/jpeg, image/png"
             onChange={uploadImage} />
         </StRowFormBox>
+        <StFormValidation validationCount={validationMessage}>
+          아래의 내용을 입력해주세요! <br />
+          "{validationMessage.length >= 1
+          && (
+          validationMessage.map((message) => (
+            <span key={`${ message }`}>{` ${message} `}</span>
+          )))}"
+        </StFormValidation>
         <StRowFormBox>
-          <button>취소</button>
-          <button onClick={onClickAddPost}>등록</button>
+          <button
+            onClick={onClickCancel}
+          >취소</button>
+
+          <button 
+            onClick={onClickAddPost}
+          >등록</button>
         </StRowFormBox>
+        
       </StPostContentsBox>
     </StPostFormWrap>
   )
@@ -138,7 +181,7 @@ const StPostImageBox = styled.div`
   flex-direction: row;
   align-items: center;
   width: 50%;
-  min-height: 529px;
+  min-height: 600px;
   border-right: var(--border-style);
   background: no-repeat center/100px auto url(${IconImage});
   overflow: hidden;
@@ -146,11 +189,13 @@ const StPostImageBox = styled.div`
     display: block;
     width: 100%;
   }
+  .imageAlertBox {
+    color: var(--red-color);
+  }
 `
 const StPostContentsBox = styled.div`
   width: 50%;
   height: 100%;
-  /* border-left: var(--border-style); */
   padding: 3rem;
   h2 {
     border-bottom: var(--border-style);
@@ -234,17 +279,32 @@ const StRowFormBox = styled.p`
     font-weight: 500;
     border: var(--border-style);
     margin-left: 10px;
-    background-color: var(--bg-color);
+    background-color: var(--blue-color);
     cursor: pointer;
 
-    &:last-child {
-      background-color: var(--green-color);
-    }
-
     &:hover {
-      background-color: #000;
-      color:var(--bg-color);
+      background-color: var(--red-color);
+      color:#000;
+    }
+    &:last-child:hover {
+      background-color: var(--green-color);
+      color: #000;
     }
   }
-  
+`
+const StFormValidation = styled.div`
+  display: ${({validationCount}) => (validationCount.length < 1 ? 'none' : 'block')};
+  width: 100%;
+  height: 32px;
+  font-style: italic;
+  color: var(--red-color);
+  margin-top: 20px;
+  padding: 0 10px;
+  border-left: 2px solid var(--red-color);
+  overflow:hidden;
+  span {
+    color: var(--red-color);
+    font-weight: 700;
+  }
+
 `
